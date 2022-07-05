536f67192360313e6dc8e0158dc0215c3e8ddbc1
Artem Krumin <trapper.programmer@gmail.com>
2022-07-05 23:41:03 +0300

    add css

--- src/main/java/com/umin/news/controllers/MainController.java
+++ src/main/java/com/umin/news/controllers/MainController.java
@@ -3,14 +3,13 @@ package com.umin.news.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "Главная страница");
+        model.addAttribute("title", "home page");
         return "home";
     }
 
